7c24648a9160f0cb73cd8c7462d02febac51bd4d
Ernesto Menéndez <pyalec@gmail.com>
2025-06-01 20:23:07 -0600

    ParallelSimulation can spawn a single GeneticSimulation agent

--- tsp-sim-agent-parallel/src/lib.rs
+++ tsp-sim-agent-parallel/src/lib.rs
@@ -1,6 +1,15 @@
-use std::sync::atomic::AtomicBool;
-use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::Receiver;
+use std::sync::{mpsc, Arc};
+use std::thread;
+use std::thread::JoinHandle;
 use tsp_sim_agent::{GeneticSimulation, Location, Route, Simulation, SimulationEvent};
+
+struct ThreadControl {
+    event_receiver: Receiver<SimulationEvent>,
+    stop: Arc<AtomicBool>,
+    join_handle: JoinHandle<Route>,
+}
 
 #[derive(Debug, Clone)]
 pub struct ParallelSimulation {
@@ -15,8 +24,27 @@ impl Simulation for ParallelSimulation {
     where
         F: Fn(SimulationEvent),
     {
-        let sim = GeneticSimulation::from(self.clone());
-        sim.run(stop, simulation_event_callback)
+        let thread_control = self.spawn_simulation_agent();
+
+        loop {
+            let simulation_event = thread_control.event_receiver.try_recv().ok();
+
+            match simulation_event {
+                Some(SimulationEvent::Finished) => {
+                    break;
+                }
+                Some(event) => simulation_event_callback(event),
+                _ => {}
+            }
+
+            if stop.load(Ordering::Relaxed) {
+                break;
+            }
+        }
+
+        simulation_event_callback(SimulationEvent::Finished);
+        thread_control.stop.store(true, Ordering::Relaxed);
+        thread_control.join_handle.join().unwrap()
     }
 }
 
@@ -27,6 +55,25 @@ impl ParallelSimulation {
             population_size: 200,
             max_iterations: Some(100_000),
             assume_convergence: Some(25_000),
+        }
+    }
+
+    fn spawn_simulation_agent(&self) -> ThreadControl {
+        let sim = GeneticSimulation::from(self.clone());
+        let (event_sender, event_receiver) = mpsc::channel::<SimulationEvent>();
+        let stop = Arc::new(AtomicBool::new(false));
+        let stop2 = stop.clone();
+
+        let join_handle = thread::spawn(move || {
+            sim.run(&stop2, |event| {
+                event_sender.send(event).unwrap();
+            })
+        });
+
+        ThreadControl {
+            event_receiver,
+            stop,
+            join_handle,
         }
     }
 }
